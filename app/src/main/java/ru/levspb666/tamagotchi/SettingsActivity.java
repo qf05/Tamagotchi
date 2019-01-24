aa3bc455d851705bc814727b68b47d5dfd69c220
levspb666 <levspb666@yandex.ru>
2019-01-24 23:22:36 +0300

    2_2_settings_activity

---
+++ app/src/main/java/ru/levspb666/tamagotchi/SettingsActivity.java
@@ -0,0 +1,32 @@
+package ru.levspb666.tamagotchi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public void createPet(View view) {
+
+    }
+
+
+    public void changePet(View view) {
+
+    }
+
+    public void deletePet(View view) {
+
+    }
+
+    public void changeNamePet(View view) {
+
+    }
+}
