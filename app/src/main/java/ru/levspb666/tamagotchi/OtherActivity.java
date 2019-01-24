0780c0316629e07952faee97cf54f6c33088a33e
levspb666 <levspb666@yandex.ru>
2019-01-24 14:43:26 +0300

    1_2_animation

--- app/src/main/java/ru/levspb666/tamagotchi/OtherActivity.java
+++ app/src/main/java/ru/levspb666/tamagotchi/OtherActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
@@ -14,6 +17,9 @@ public class OtherActivity extends AppCompatActivity {
         setContentView(R.layout.other_activity);
         Button home = findViewById(R.id.toHome);
         home.setOnClickListener(onClickListener);
+        TextView textView = findViewById(R.id.hello);
+        final Animation helloAnimation = AnimationUtils.loadAnimation(this, R.anim.hello);
+        textView.startAnimation(helloAnimation);
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
