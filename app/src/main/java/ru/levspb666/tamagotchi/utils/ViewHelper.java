9ff10abb21decad25d6708ea3a8023ebea752298
levspb666 <levspb666@yandex.ru>
2019-01-24 14:55:17 +0300

    1_3_translate_animation

---
+++ app/src/main/java/ru/levspb666/tamagotchi/utils/ViewHelper.java
@@ -0,0 +1,19 @@
+package ru.levspb666.tamagotchi.utils;
+
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+public class ViewHelper {
+    public static void executeAfterViewHasDrawn(final View v, final Runnable cb) {
+        ViewTreeObserver vto = v.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                cb.run();
+                ViewTreeObserver obs = v.getViewTreeObserver();
+                obs.removeOnGlobalLayoutListener(this);
+            }
+        });
+    }
+}
