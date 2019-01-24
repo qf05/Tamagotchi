527ce49275f216a19052d4f50690e5b6bcf6b66d
levspb666 <levspb666@yandex.ru>
2019-01-24 15:11:41 +0300

    HW1_0_buttons

--- app/src/main/java/ru/levspb666/tamagotchi/MainActivity.java
+++ app/src/main/java/ru/levspb666/tamagotchi/MainActivity.java
@@ -4,14 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button cat;
+    Button dog;
+    Button cthulhu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cat = findViewById(R.id.buttonCat);
+        dog = findViewById(R.id.buttonDog);
+        cthulhu = findViewById(R.id.buttonCthulhu);
+        cat.setOnClickListener(buttonListener);
+        dog.setOnClickListener(buttonListener);
+        cthulhu.setOnClickListener(buttonListener);
     }
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.buttonCat:
+                    break;
+                case R.id.buttonDog:
+                    break;
+                case R.id.buttonCthulhu:
+                    break;
+            }
+        }
+    };
 
     public void onButtonClick(View view) {
         Intent intent = new Intent(MainActivity.this, OtherActivity.class);
